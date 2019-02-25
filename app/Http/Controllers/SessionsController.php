b668c322d2f27402db69d1c0760588cae08b2c57
panpan <dp19891120@qq.com>
2019-02-25 08:43:39 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 use Auth;
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create(){
     	if(Auth::user()){
     		return redirect()->route('users.show',[Auth::user()]);
@@ -22,7 +29,9 @@ class SessionsController extends Controller
 
        if (Auth::attempt($credentials,$request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+          // return redirect()->route('users.show', [Auth::user()]);
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
@@ -30,7 +39,7 @@ class SessionsController extends Controller
     }
 
     public function destroy(){
-    	Auth::logout();
+    	  Auth::logout();
         session()->flash('success', '您已成功退出！');
         return redirect('login');
     }
